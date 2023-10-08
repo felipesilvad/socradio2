04bd0175b9510031af69ee51450f3bf470961750
unknown <felipe.silvad@outlook.com>
2023-10-08 01:43:23 -0300

    up

--- src/Components/SignIn.js
+++ src/Components/SignIn.js
@@ -1,12 +1,37 @@
-import React from 'react';
-import {auth} from '../firebase';
+import React, {useState,useEffect} from 'react';
+import {auth,firestore} from '../firebase';
 import firebase from 'firebase/compat/app';
+import {doc,setDoc,query,collection,onSnapshot,where} from "firebase/firestore";
 
 function SignIn() {
+  const [users, setUsers] = useState('')
+  useEffect (() => {
+    onSnapshot(query(collection(firestore, `/users`)), (snapshot) => {
+      setUsers(snapshot.docs.map(doc => (doc.data())))
+    });
+  }, [])
+
+  console.log(users)
 
   const signInWithGoogle = () => {
     const provider = new firebase.auth.GoogleAuthProvider();
-    auth.signInWithPopup(provider);
+    const usersRef = firestore.collection('users');
+
+    auth.signInWithPopup(provider).then(async (result) => {
+      usersRef.doc(result.user.uid).set({
+        createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+        uid: result.user.uid,
+        email: result.user.email
+      })
+      // setDoc(doc(db, "users", userCredential.user.uid), {
+      //   id: userCredential.user.uid,
+      //   email: email,
+      //   username: username,
+      //   profile_pic: selectedPic,
+      //   profile_pic_url: selectedPicUrl,
+      //   createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      // })
+    })
   }
 
   return (
