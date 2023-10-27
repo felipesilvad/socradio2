f3d04b27c1930c52f60b3111105e6cd65e515679
felipe <felipe.silvad@outlook.com>
2023-10-27 14:24:34 -0300

    up

--- src/Components/AudioPlayer/AudioPlayerComponent.js
+++ src/Components/AudioPlayer/AudioPlayerComponent.js
@@ -6,7 +6,7 @@ import {firestore} from '../../firebase';
 import {Image} from 'react-bootstrap';
 import { doc, getDoc, onSnapshot } from "firebase/firestore";
 
-function AudioPlayerComponent() {
+function AudioPlayerComponent({station}) {
   const player = useRef()
 
   const songsRef = firestore.collection('songs');
@@ -14,6 +14,8 @@ function AudioPlayerComponent() {
   // const [songList] = useCollectionData(query, { idField: 'id' });
   // const [currentSongIndex, setCurrentSongIndex] = useState(0);
   const [currentSong, setCurrentSong] = useState();
+  const [currentSongIndex, setCurrentSongIndex] = useState();
+  const [playlist, setPlaylist] = useState();
 
   const setCurrentSongFromDB = (songID) => {
     onSnapshot(doc(firestore, "/songs/", songID), (doc) => {
@@ -34,24 +36,41 @@ function AudioPlayerComponent() {
   };
 
   const setCurrentSongFromAPI = async () => {
-    const response = await fetch("http://127.0.0.1:5000/currentSong")
+    const response = await fetch(`http://127.0.0.1:5000/currentSong${station}`)
     const song = await response.json();
     setCurrentSong(song)
+    setCurrentSongIndex(song.index)
+    console.log(song)
   }
 
-  const LoadSong =  async () => {
-    setCurrentSongFromAPI()
-    const now = new Date().getTime()
-    const seconds = now / 1000
-    const songStart = await currentSong.startTime
-    toTime(seconds-songStart)
+
+  const setPlaylistSongFromAPI = async () => {
+    const response = await fetch(`http://127.0.0.1:5000/playlist${station}`)
+    const playlist = await response.json();
+    setPlaylist(playlist)
+  }
+
+  const onEndedSong = () => {
+    setCurrentSong(playlist[currentSongIndex+1])
+    setCurrentSongIndex(currentSongIndex+1)
   }
 
   useEffect(() => {
-    LoadSong()
+    setCurrentSongFromAPI()
+    setPlaylistSongFromAPI()
   }, [])
 
-  console.log(currentSong)
+  useEffect(() => {
+    if (currentSong) {
+      const now = new Date().getTime()
+      const seconds = now / 1000
+      const songStart =  currentSong.startTime
+      toTime(seconds-songStart)
+    }
+  }, [currentSong])
+
+  // console.log(playlist)
+  // console.log([currentSongIndex])
   
   if (currentSong&&currentSong.data) {
     return (
@@ -74,7 +93,7 @@ function AudioPlayerComponent() {
               src={currentSong.data.audio}
               onPlay={e => console.log("onPlay")}
               ref={player}
-              onEnded={() => LoadSong()}
+              onEnded={() => onEndedSong()}
               showDownloadProgress={false}
               showJumpControls={false}
               hasDefaultKeyBindings={false}
